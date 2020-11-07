bf78b18ad13c4c98bf50f7a82e3c59522deb1f1f
Mayro <Mayro@MAYRONMARTINS>
2020-11-07 16:10:35 -0300

    Finalizaçao de Turmas

--- src/View/Menu.java
+++ src/View/Menu.java
@@ -167,7 +167,7 @@ public class Menu extends javax.swing.JFrame {
     private void botaoTurmasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoTurmasActionPerformed
         // TODO add your handling code here:
         this.dispose();
-        Turmas abrir=new Turmas();
+        TurmasView abrir=new TurmasView();
         abrir.setVisible(true);
     }//GEN-LAST:event_botaoTurmasActionPerformed
 
